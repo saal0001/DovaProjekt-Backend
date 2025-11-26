f862b927e51a7ad19c6e2e0b23ab7dd5569ad7fa
saal0001 <saal0001@stud.kea.dk>
2025-11-26 16:23:13 +0100

    Initial commit

---
+++ src/main/java/com/example/dovaprojektbackend/DovaProjektBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dovaprojektbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DovaProjektBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DovaProjektBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dovaprojektbackend/DovaProjektBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dovaprojektbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DovaProjektBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
