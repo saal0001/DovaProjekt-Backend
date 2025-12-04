b0a97db733673a504a5a381038578702e3e7befe
Mohamed Adan <moom0001@stud.ek.dk>
2025-12-04 16:44:37 +0100

    Er ikke helt færdig i nu

---
+++ src/main/java/com/example/dovaprojektbackend/enums/Role.java
@@ -0,0 +1,7 @@
+package com.example.dovaprojektbackend.enums;
+
+public enum Role {
+    CUSTOMER,
+    ADMIN,
+    SHOP
+}
---
+++ src/main/java/com/example/dovaprojektbackend/model/User.java
@@ -0,0 +1,99 @@
+package com.example.dovaprojektbackend.model;
+
+import com.example.dovaprojektbackend.enums.Role;
+import jakarta.persistence.*;
+import org.hibernate.annotations.processing.Pattern;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true,  nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public User(){}
+
+    public User(String name, String email, String password, String phone, String address, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
